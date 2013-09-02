0db6a9fe4871d7eccb9da15c659d0fb9eda683bc
Fajran Iman Rusadi <fajran@gmail.com>
2013-09-02 14:56:58 +0200

    Add connection string parser
    Format:
    
       [user[:password]@]hostname[:port]/database

--- driver.go
+++ driver.go
@@ -3,6 +3,9 @@ package monetdb
 import (
 	"database/sql"
 	"database/sql/driver"
+	"fmt"
+	"regexp"
+	"strconv"
 )
 
 func init() {
@@ -21,13 +24,38 @@ type config struct {
 }
 
 func (*Driver) Open(name string) (driver.Conn, error) {
+	c, err := parseDSN(name)
+	if err != nil {
+		return nil, err
+	}
+	return newConn(c)
+}
+
+func parseDSN(name string) (config, error) {
+	re := regexp.MustCompile(`((?P<username>.+?)(:(?P<password>.+?))?@)?(?P<hostname>.+?)(:(?P<port>\d+?))?/(?P<database>.+?)`)
+	if !re.MatchString(name) {
+		return config{}, fmt.Errorf("Invalid DSN")
+	}
+	m := re.FindAllStringSubmatch(name, -1)[0]
+	n := re.SubexpNames()
+
 	c := config{
-		Username: "voc",
-		Password: "voc",
 		Hostname: "localhost",
-		Database: "voc",
 		Port:     50000,
 	}
+	for i, v := range m {
+		if n[i] == "username" {
+			c.Username = v
+		} else if n[i] == "password" {
+			c.Password = v
+		} else if n[i] == "hostname" {
+			c.Hostname = v
+		} else if n[i] == "port" {
+			c.Port, _ = strconv.Atoi(v)
+		} else if n[i] == "database" {
+			c.Database = v
+		}
+	}
 
-	return newConn(c)
+	return c, nil
 }
