98c3e81141052974b28ba77401b6d74231268648
Fajran Iman Rusadi <fajran@gmail.com>
2013-09-02 22:49:38 +0200

    Add package documentation

---
+++ doc.go
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/*
+
+Package monetdb contains a database driver for MonetDB.
+
+Use the following format for the Data Source Name (DSN) to make connection
+to the MonetDB server.
+
+    [username[:password]@]hostname[:port]/database
+
+If the port is not specified, then the default port 50000 will be used.
+
+Please check the project's GitHub page for more complete documentation -
+https://github.com/fajran/go-monetdb
+
+*/
+package monetdb
