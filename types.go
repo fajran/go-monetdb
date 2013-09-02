038c09b57aa94a32fcc2e0a20bd9eeab3e57fae5
Fajran Iman Rusadi <fajran@gmail.com>
2013-09-02 22:11:04 +0200

    Add documentation

--- types.go
+++ types.go
@@ -9,29 +9,37 @@ import (
 	"time"
 )
 
+// Time represents MonetDB's Time datatype.
 type Time struct {
 	Hour, Min, Sec int
 }
 
+// Time represents MonetDB's Date datatype.
 type Date struct {
 	Year  int
 	Month time.Month
 	Day   int
 }
 
+// String returns a string representation of a Time
+// in the form "HH:YY:MM".
 func (t Time) String() string {
 	return fmt.Sprintf("%02d:%02d:%02d", t.Hour, t.Min, t.Sec)
 }
 
+// String returns a string representation of a Date
+// in the form "YYYY-MM-DD"
 func (d Date) String() string {
 	return fmt.Sprintf("%04d-%02d-%02d", d.Year, d.Month, d.Day)
 }
 
+// GetTime takes the clock part of a time.Time and put it in a Time
 func GetTime(t time.Time) Time {
 	hour, min, sec := t.Clock()
 	return Time{hour, min, sec}
 }
 
+// GetDate takes the date part of a time.Time and put it in a Date
 func GetDate(t time.Time) Date {
 	year, month, day := t.Date()
 	return Date{year, month, day}
